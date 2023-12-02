af1cd703402d83c9c3e5ad39ec022ff1aa6821da
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-02 17:15:24 +0100

    initialized repositor

---
+++ src/main/java/dev/ali/alg/App.java
@@ -0,0 +1,16 @@
+package dev.ali.alg;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        int num1, num2, resultado;
+        num1 = 4;
+        num2 = 2;
+
+        resultado = num1 * num2;
+
+        System.out.println("El resultado es:" + resultado);
+    }
+}
